04eaf35feaf6c50244b7f917fb1e4fcd0f19aff0
Madlena Calugareanu <madlena.calugareanu@bearingpoint.com>
2024-03-01 10:03:55 +0200

    changes to wishlist

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.scss']
+  styleUrls: ['./general.component.scss'],
 })
 export class GeneralComponent {
+  menuVisible = true;
 
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+    this.toggleMenu();
+  }
 }
